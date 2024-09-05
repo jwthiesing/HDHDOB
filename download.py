698fd0eec8de81e9ac9bbcb3940dbb2cfcbe7bcd
CocoasColas <149021286+CocoasColas@users.noreply.github.com>
2024-09-04 21:34:28 -0500

    Update download.py

--- download.py
+++ download.py
@@ -1,73 +1,70 @@
-def downloadstorm(name, year):
-	from urllib.request import urlopen
-	import requests
-	import pandas as pd
-	from io import StringIO
-	import os
-	#targetdt = bt_hour_rounder(dt.datetime(year=year, month=mon, day=day, hour=hour, minute=minute))
-	#year = int(str(year)[2:])
-	#mon = str(mon).rjust(2, '0')
-	#day = str(day).rjust(2, '0')
-	#storm = storm.upper()[:4] + storm.upper()[6:]
-	name = name.lower()
-	#x = str(targetdt.hour).rjust(2, '0')
-	#print(storm, year, mon, day, x)
+def downloadstorm(name, year, return_file_names=False):
+    from urllib.request import urlopen
+    import requests
+    import pandas as pd
+    from io import StringIO
+    import os
+    import re
 
-	directory = f'https://www.aoml.noaa.gov/ftp/pub/hrd/data/flightlevel/{year}/{name}/'
-	page = requests.get(directory).text
-	content = page.split("\n")
-	files = []
-	for line in content:
-		if ".1sec.txt" in line or ".1sec" in line:
-			#print(line)
-			files.append(
-				((line.split('"> ')[1]).split("</a>")[0]))
-	del content
-	print(files)
-	if len(files) < 1:
-		import sys
-		sys.exit("No valid files found in directory. Exiting.")
-	#files = sorted([i for i in files if i[1][:8]
-	#			   in timestr], key=lambda x: x[1])
-	#linksub = [archive_url[0] + '.'.join(l) for l in files]
-	textlist = []
-	for ii, file in enumerate(files):
-		if not os.path.isfile('./Data/'+file):
-			#if not os.path.exists(directory+'/'+file):
-				#os.makedirs(DOWNDIR+'/'.join(objectname.split('/')[0:4]))
-			try:
-				with urlopen(directory+file) as url:
-					print(f"Downloading file {ii}")
-					urldec = url.read().decode('utf-8')
-					textlist.append(urldec)
-					with open('./Data/'+file, "w", encoding="utf-8") as f:
-						f.write(urldec)
-					
-			except:
-				continue
-		with open('./Data/'+file, "r") as f:
-			textlist.append(f.read())
+    name = name.lower()
+    directory = f'https://www.aoml.noaa.gov/ftp/pub/hrd/data/flightlevel/{year}/{name}/'
+    page = requests.get(directory).text
+    content = page.split("\n")
+    files = []
+    for line in content:
+        if ".1sec.txt" in line or ".1sec" in line:
+            files.append((line.split('"> ')[1]).split("</a>")[0])
 
-	#print(textlist[9].split('\n')[15700])
-	#print(textlist[9].split('\n')[15701])
-	#print(textlist[9].split('\n')[15702])
-	#print(textlist[9].split('\n')[15703])
-	#print(textlist[9].split('\n')[15704])
-	#print(textlist[9].split('\n')[15705])
-	#print(textlist[9].split('\n')[15706])
-	#print(textlist[9].split('\n')[15707])
+    if len(files) < 1:
+        import sys
+        sys.exit("No valid files found in directory. Exiting.")
 
-	dflist = []
-	for ii, text in enumerate(textlist):
-		print(f"Parsing file {ii}")
-		try:
-			dflist.append(pd.read_csv(StringIO(text), sep="\s+", skiprows=[0, 1, 3], header=0, skipfooter=1, on_bad_lines='warn'))
-		except:
-			continue
-	#print(dflist[0])
-	return dflist
+    textlist = []
+    for ii, file in enumerate(files):
+        file_path = f'./Data/{file}'
+        if not os.path.isfile(file_path):
+            try:
+                with urlopen(directory+file) as url:
+                    print(f"Downloading file {ii + 1}")
+                    urldec = url.read().decode('utf-8')
+                    textlist.append(urldec)
+                    with open(file_path, "w", encoding="utf-8") as f:
+                        f.write(urldec)
+            except:
+                continue
+        else:
+            print(f"Reading existing file {ii + 1}")
+            with open(file_path, "r") as f:
+                textlist.append(f.read())
+
+    dflist = []
+    for ii, text in enumerate(textlist):
+        print(f"Parsing file {ii + 1}")
+        try:
+            cleaned_lines = []
+            
+            for line in text.splitlines():
+                fields = line.split()
+                if len(fields) > 1 and fields[1] == '0.000' and fields[2] == '0.000':
+                    continue
+
+                cleaned_fields = [field for field in fields if not (re.fullmatch(r'\b\d\b', field))]
+                cleaned_line = ' '.join(cleaned_fields)
+                cleaned_lines.append(cleaned_line)
+
+            cleaned_text = '\n'.join(cleaned_lines)
+            df = pd.read_csv(StringIO(cleaned_text), sep="\s+", skiprows=[0, 1, 3], header=0, skipfooter=1, on_bad_lines='warn', engine='python')
+            dflist.append(df)
+
+        except Exception as e:
+            print(f"Error parsing file {ii + 1}: {e}")
+            continue
+
+    if return_file_names:
+        return dflist, files
+    return dflist
 
 if __name__ == "__main__":
-	name = "Allen"
-	year = 1980
-	downloadstorm(name, year)
+    name = "Allen"
+    year = 1980
+    downloadstorm(name, year)
